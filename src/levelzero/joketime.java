b500a328257c5f1c9340a4ad4fbb589f19dc0f08
league-workshopper-4 <workshop@Mac-mini.local>
2015-09-09 17:58:45 -0700

    yay
    jeff code

---
+++ src/levelzero/joketime.java
@@ -0,0 +1,13 @@
+package levelzero;
+
+import javax.swing.JOptionPane;
+
+public class joketime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "");
+
+	}
+
+}
---
+++ src/levelzero/robotgrafitti.java
@@ -0,0 +1,18 @@
+package levelzero;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class robotgrafitti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Robot Jeff=new Robot(); 
+		Jeff.penDown();
+		Jeff.move(300);
+Jeff.turn(135);
+Jeff.move(400);
+Jeff.turn(225);
+Jeff.move(300);
+	}
+
+}
