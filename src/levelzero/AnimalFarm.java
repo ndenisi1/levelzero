f3f1477a1aec2cc6d01140cb07868bcadf502d64
league-workshopper-1 <workshop@Mac-mini.local>
2015-10-21 18:30:45 -0700

    yay

---
+++ src/levelzero/AnimalFarm.java
@@ -0,0 +1,64 @@
+package levelzero;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class AnimalFarm {
+
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that
+		 * animal.
+		 */
+String meow = JOptionPane.showInputDialog("What animal do you like?"); 
+		/* 2. Make it so that the user can keep entering new animals. */
+if (meow.equalsIgnoreCase("dog")){
+playWoof();}
+else if(meow.equals("cow")){
+	playMoo();
+}
+
+}
+
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	  String quackFile = "/Users/workshop/Google Drive/league-sounds/quack.wav";
+	String mooFile = "/Users/workshop/Google Drive/league-sounds/moo.wav";
+	String woofFile = "/Users/workshop/Google Drive/league-sounds/woof.wav";
+	String meowFile = "/Users/workshop/Google Drive/league-sounds/meow.wav";
+	String llamaFile = "/Users/workshop/Google Drive/league-sounds/llama.wav";
+
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+    	try {
+   		 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundFile));
+   		 Clip clip = AudioSystem.getClip();
+   		 clip.open(audioInputStream);
+   		 clip.start();
+   		 Thread.sleep(3400);
+    	} catch (Exception ex) {
+        	ex.printStackTrace();
+    	}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
+
+}
+
