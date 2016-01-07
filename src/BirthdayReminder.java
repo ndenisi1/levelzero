d5b9c63d65e0562e05b9e9187a9c0ab55f40bdac
Ryon <league@Mac-mini-3.local>
2016-01-06 18:26:46 -0800

    i completed my code

--- src/BirthdayReminder.java
+++ src/BirthdayReminder.java
@@ -6,31 +6,25 @@ public class BirthdayReminder {
 
 	public static void main(String[] args) {
 
-		
 		String momsBirthday = "August 26th";
 		String dadsBirthday = "May 2nd";
 		String myBirthday = "February 22nd";
 
-		
 		String hello = JOptionPane.showInputDialog("Who's birthday are you looking for?");
-		
+
 		JOptionPane.showMessageDialog(null, hello);
-		
+
 		if (hello.equalsIgnoreCase("mom")) {
 			JOptionPane.showMessageDialog(null, momsBirthday);
 		}
-		
+
 		else if (hello.equalsIgnoreCase("dad")) {
 			JOptionPane.showMessageDialog(null, dadsBirthday);
-		}
-		else if(hello.equalsIgnoreCase("noelani")){
+		} else if (hello.equalsIgnoreCase("noelani")) {
 			JOptionPane.showMessageDialog(null, myBirthday);
-		}
-		else {
+		} else {
 			JOptionPane.showMessageDialog(null, "sorry , i don't remember that person's birthday!");
 		}
-
-		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
 
 	}
 }
--- src/YearsAlive.java
+++ src/YearsAlive.java
@@ -1,9 +1,9 @@
-
+import javax.swing.JOptionPane;
 
 public class YearsAlive {
  public static void main(String[] args) {
-	for (int i = 2004; i < ; i++) {
-		
+	for (int i = 2004; i < 2017; i++) {
+		JOptionPane.showMessageDialog(null, " I was alive in "+ i);
 	}
 }
 }
