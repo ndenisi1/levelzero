a4a59d51504ade2cd800946495e7d1639e4dba73
Savera <Level 2>
2016-05-18 18:50:28 -0700

    bhjefssjkefjk

---
+++ src/levelzero/AmazingRings.java
@@ -0,0 +1,5 @@
+package levelzero;
+
+public class AmazingRings {
+
+}
---
+++ src/levelzero/badgers.java
@@ -0,0 +1,19 @@
+package levelzero;
+
+public class badgers {
+public static void main(String[] args) {
+	for (int n= 0; n < 3; n++) {
+		
+	
+	for (int i = 0; i<11; i++) {
+		System.out.println("badgers");
+	}
+	for (int i = 0; i < 2; i++) {
+		System.out.println("mushroom");
+	}
+	}
+	
+	System.out.println("SNAKKKKKEEEE");
+		
+}
+}
